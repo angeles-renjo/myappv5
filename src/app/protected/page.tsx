f92a5d1f843571eef04f6cfe9bd2b10724884d56
renjo <angeles.renjo@gmail.com>
2024-04-05 15:00:42 +1300

    Add Trade model to prisma schema and update protected page

--- src/app/protected/page.tsx
+++ src/app/protected/page.tsx
@@ -2,10 +2,15 @@
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
+import { User, Prisma } from "@prisma/client";
 
+const userEmail: Prisma.UserSelect = {
+  email: true,
+};
 export default async function ProtectedRoute() {
   const session = await getServerSession(authOptions);
   if (!session || !session.user) redirect("/");
+  console.log(session);
 
-  return <div></div>;
+  return <div>Protected</div>;
 }
